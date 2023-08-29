ba48207577b4b66652776550fb094796ba26122d
salahukutipuravan@gmail.com <salahukutipuravan@gmail.com>
2023-08-29 11:09:00 +0530

    session 2

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+//PascalCasing
+function Message(){
+    //JSX:  JavaScrip[t XML
+    const name = 'Salah'
+    if(name)
+        return <h1>Hello Wolrd!,{name} Welcome to wolrd of programming</h1>
+    return <h1>Hello world</h1>
+}
+
+export default Message;
